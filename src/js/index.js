cd923a54d0cf52977dcba9cdb19612dc6de3c234
Александр <westerovs@mail.ru>
2021-06-27 10:52:38 +0300

    create

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+const render = (container, template, place = 'beforeend') => {
+    if (container instanceof Element) {
+        container.insertAdjacentHTML(place, template)
+    }
+}
+console.log('test template')
+
